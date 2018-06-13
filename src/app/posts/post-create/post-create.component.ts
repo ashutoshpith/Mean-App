006729593ed21075229e9797029625ebc8f9ccce
ashutosh <amxdp18@gmail.com>
2018-06-13 13:08:00 +0530

    added angular eventEmiiter

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-post-create',
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class PostCreateComponent implements OnInit {
   enterContent = '';
   enterTitle = '';
+  @Output() postCreated = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
@@ -18,6 +19,7 @@ export class PostCreateComponent implements OnInit {
       title: this.enterTitle,
       content: this.enterContent
     };
+    this.postCreated.emit(post);
   }
 
 }
