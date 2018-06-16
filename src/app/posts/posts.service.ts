0ff10024df245386abfa60d3fbc2809ca15931b6
ashutosh <amxdp18@gmail.com>
2018-06-16 17:24:16 +0530

    form improvment

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { Post } from './post.model';
 import { HttpClient } from '@angular/common/http';
-import { ThrowStmt } from '@angular/compiler';
 
 @Injectable({providedIn: 'root'})
 
